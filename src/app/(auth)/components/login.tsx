57caa0d2fdcac055dfd472fc7677adfb5e4f9c27
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2024-04-22 10:24:07 +0000

    feat: add validation for auth forms

--- src/app/(auth)/components/login.tsx
+++ src/app/(auth)/components/login.tsx
@@ -1,17 +1,64 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import { EyeOff, Eye } from "lucide-react";
+import React, { FormEvent, useState } from "react";
+import { toast } from "sonner";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!email.trim() || !password.trim()) {
+      toast.error("One or more fields empty");
+      return;
+    }
+    setLoading(true);
+
+    // Perform form submission logic here
+  };
   return (
     <div className="py-4 space-y-4">
-      <div className="w-full space-y-2">
+      <form onSubmit={handleSubmit} className="w-full space-y-2">
         <label htmlFor="">Email</label>
-        <Input />
-      </div>
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </form>
       <div className="w-full space-y-2">
         <label htmlFor="">Password</label>
-        <Input />
+        <div className="w-full relative">
+          <Input
+            type={showPassword ? "text" : "password"}
+            key="password"
+            id="password"
+            name="password"
+            value={password}
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="absolute right-4 top-2"
+          >
+            {showPassword ? <EyeOff /> : <Eye />}
+          </button>
+        </div>
       </div>
       <Button className="bg-black w-full text-white">Sign in</Button>
       <div className="flex items-center gap-2">
--- src/app/(auth)/components/signup.tsx
+++ src/app/(auth)/components/signup.tsx
@@ -1,37 +1,188 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import { EyeOff, Eye, LoaderCircle } from "lucide-react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "sonner";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    invalidLength: "",
+    unmatchedPassword: "",
+  });
+  const { firstName, lastName, email, password, confirmPassword } = formData;
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (
+      !firstName.trim() ||
+      !lastName.trim() ||
+      !email.trim() ||
+      !password.trim() ||
+      !confirmPassword.trim()
+    ) {
+      toast.error("One or more fields empty");
+      return;
+    }
+    setLoading(true);
+    console.log(formData);
+    // Perform form submission logic here
+  };
+  const validatePassword = (password: string) => {
+    if (password.length < 6) {
+      setErrors((prev) => ({
+        ...prev,
+        invalidLength: "Password should be at least 6 characters long",
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        invalidLength: "",
+      }));
+    }
+  };
+
+  const matchPassword = (confirmPassword: string) => {
+    if (password.trim() !== confirmPassword.trim()) {
+      setErrors((prev) => ({
+        ...prev,
+        unmatchedPassword: "Passwords do not match",
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        unmatchedPassword: "",
+      }));
+    }
+  };
+
   return (
     <div className="py-4 space-y-4">
-      <div className="w-full space-y-2">
-        <label htmlFor="">First Name</label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <label htmlFor="">Last Name</label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <label htmlFor="">Email</label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <label htmlFor="">Password</label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <label htmlFor="">Confirm Password</label>
-        <Input />
-      </div>
-      <Button className="bg-black w-full text-white">Sign up</Button>
+      <form onSubmit={handleSubmit}>
+        <div className="w-full space-y-2">
+          <label htmlFor="firstName">First Name</label>
+          <Input
+            type="text"
+            id="firstName"
+            name="firstName"
+            required
+            value={firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="w-full space-y-2">
+          <label htmlFor="lastName">Last Name</label>
+          <Input
+            type="text"
+            id="lastName"
+            required
+            name="lastName"
+            value={lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="w-full space-y-2">
+          <label htmlFor="email">Email</label>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="w-full space-y-2">
+          <label htmlFor="password">Password</label>
+          <div className="w-full relative">
+            <Input
+              type={showPassword ? "text" : "password"}
+              key="password"
+              id="password"
+              required
+              name="password"
+              onBlur={(e) => validatePassword(e.target.value)}
+              value={password}
+              onChange={(e) => {
+                handleChange(e);
+                validatePassword(e.target.value);
+              }}
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute right-4 top-2"
+            >
+              {showPassword ? <EyeOff /> : <Eye />}
+            </button>
+          </div>
+
+          {errors.invalidLength && (
+            <small className="text-red-500">{errors.invalidLength}</small>
+          )}
+        </div>
+        <div className="w-full space-y-2">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <div className="w-full relative">
+            <Input
+              type={showPassword ? "text" : "password"}
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              required
+              onBlur={(e) => matchPassword(e.target.value)}
+              onChange={(e) => {
+                handleChange(e);
+                matchPassword(e.target.value);
+              }}
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute right-4 top-2"
+            >
+              {showPassword ? <EyeOff /> : <Eye />}
+            </button>
+          </div>
+          {errors.unmatchedPassword && (
+            <small className="text-red-500">{errors.unmatchedPassword}</small>
+          )}
+        </div>
+        <Button type="submit" className="bg-black w-full text-white">
+          {loading ? <LoaderCircle className="animate-spin" /> : "Sign up"}
+        </Button>
+      </form>
       <div className="flex items-center gap-2">
         <div className="h-[1px] bg-gray-300 w-full" />
         or
         <div className="h-[1px] bg-gray-300 w-full" />
       </div>
-      <Button className="bg-black w-full text-white">Continue with Google</Button>
+      <Button type="button" className="bg-black w-full text-white">
+        Continue with Google
+      </Button>
     </div>
   );
 }
