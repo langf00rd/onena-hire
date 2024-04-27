c614a8ab0aea25fd252c2044b3ce4cd196c7f8cf
langford <langfordquarshie21@gmail.com>
2024-04-27 09:00:55 +0000

    fix(mobile): application submitted ui (#11)
    * chore: [WIP] UI
    
    * chore: [WIP] UI
    
    * chore: mobile view stuff
    
    * chore: create job post UI [WIP]
    
    * [WIP] fetch and create job post (#4)
    
    * feat: [WIP] build job application input fields
    
    * feat: [WIP] fetch user job posts
    
    * chore: reinstall packages
    
    * fix: remove unused import
    
    * add auth ui and integrate support with added email endpoint (#1)
    
    * feat: add auth pages ui
    
    * feat: add validation for auth forms
    
    * feat: add email endpoint and integrate in support
    
    * chore: give auth pages dedicated route
    
    * feat: add auth phase 1 supabase
    
    * chore: resolve merge conf
    
    * chore: add example env
    
    * feat: add auth and middleware
    
    * chore: handle errors + remove google auth
    
    * fix: build errors
    
    * chore: reinstall packages
    
    ---------
    
    Co-authored-by: langford <langfordquarshie21@gmail.com>
    
    * mvp (#5)
    
    * feat: [WIP] build job application input fields
    
    * feat: [WIP] fetch user job posts
    
    * chore: reinstall packages
    
    * fix: remove unused import
    
    * chore: MVP works
    
    * fix: only show an organization's job posts
    
    * refactor: update env.example
    
    * refactor: remove console logs (#6)
    
    * feat: [WIP] build job application input fields
    
    * feat: [WIP] fetch user job posts
    
    * chore: reinstall packages
    
    * fix: remove unused import
    
    * chore: MVP works
    
    * fix: only show an organization's job posts
    
    * refactor: update env.example
    
    * chore: minor stuff
    
    * feat: shareable job post link (#8)
    
    * feat: [WIP] build job application input fields
    
    * feat: [WIP] fetch user job posts
    
    * chore: reinstall packages
    
    * fix: remove unused import
    
    * chore: MVP works
    
    * fix: only show an organization's job posts
    
    * refactor: update env.example
    
    * chore: minor stuff
    
    * feat: shareable job post link
    
    * feat: shareable job post link
    
    * fix(mobile): application submitted ui
    
    fix font sizing and padding for mobile
    
    ---------
    
    Co-authored-by: Stephen <105071093+stephenokyere233@users.noreply.github.com>

--- src/app/organization/[domain]/jobs/[jobID]/submitted/page.tsx
+++ src/app/organization/[domain]/jobs/[jobID]/submitted/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <h1 className="text-3xl">Your application has been submitted 🎉</h1>
+    <div className="w-screen px-10 h-screen flex items-center justify-center">
+      <h1 className="text-xl text-center md:text-3xl">
+        Your application has been submitted 🎉
+      </h1>
     </div>
   );
 }
