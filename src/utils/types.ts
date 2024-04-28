7836cb58f40f7bcb6e46f0dbf2d0e4bc17ec85cc
langford <langfordquarshie21@gmail.com>
2024-04-28 18:31:29 +0000

    search on career page (#18)
    * feat: export application emails
    
    emails in applications can now be exported and automatically downloaded as a txt file
    
    * feat: search on career page

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -104,6 +104,8 @@ export interface JobPost {
   organization: number;
   organizations: { name: string; domain: string };
   input_fields: InputFieldComponentProps[];
+  location_type: string;
+  location?: string;
   [key: string]: unknown;
 }
 
