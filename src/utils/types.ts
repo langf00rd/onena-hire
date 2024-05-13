5cc12531bd1685b5ede05a9dc7e5fda08d3468ee
langford <langfordquarshie21@gmail.com>
2024-05-13 06:55:43 +0000

    Update types.ts (#26)

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,3 @@
-import { FILE_TYPES, INPUT_FIELD_TYPES } from "@/utils/constants";
-
 export interface Application {
   [key: string]: unknown;
   id: string;
@@ -28,68 +26,6 @@ export interface PageProps {
   // searchParams: Record<string, unknown>;
   params: Record<string, unknown>;
   searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export interface JobPost {
-  id: number;
-  poster: number;
-  description: string;
-  created_at: string;
-  updated_at?: string;
-  role: string;
-  requirements: string[];
-  responsibilities: string[];
-  organization: number;
-  organizations: { name: string; domain: string };
-  input_fields: InputFieldComponentProps[];
-  location_type: string;
-  location?: string;
-  [key: string]: unknown;
-}
-
-export type ApplicationJobPost = Omit<
-  JobPost,
-  "id" | "updated_at" | "created_at" | "organizations" | "input_fields"
->;
-
-export interface DBUser {
-  id: number;
-  created_at: string;
-  updated_at: string;
-  email: string;
-  first_name: string;
-  last_name: string;
-  organization: number;
-}
-
-// export interface JobPost {
-//   role: string;
-//   description: string;
-//   responsibilities: string[];
-//   requirements: string[];
-//   organization: number;
-//   custom_sections: string;
-//   input_fields: string;
-//   poster: number;
-// }
-
-export interface InputFieldComponentProps {
-  id: string;
-  type: string;
-  label: string;
-  maxChars?: number;
-  required?: boolean;
-  file_field_type?: string;
-}
-
-export interface Organization {
-  id: number;
-  updated_at?: string;
-  created_at: string;
-  employees_num: string;
-  name: string;
-  website?: string;
-  domain: string;
 }
 
 export interface JobPost {
