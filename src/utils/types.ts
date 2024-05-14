eca9348f7e37933ab6fd5c0f5f36c3ddaacf1853
langford <langfordquarshie21@gmail.com>
2024-05-14 17:23:20 +0000

    feat: view application details in sidebar

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,13 +1,13 @@
 export interface Application {
   [key: string]: unknown;
   id: string;
-  email: string;
-  first_name: string;
-  last_name: string;
-  resume_link: string;
-  cover_letter?: string;
+  // email: string;
+  // first_name: string;
+  // last_name: string;
+  // resume_link: string;
+  // cover_letter?: string;
   created_at: string;
-  time_spent: number;
+  // time_spent: number;
   location: {
     country: string;
     city: string;
